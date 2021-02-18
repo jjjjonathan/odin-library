5f924597361ffca8592faad9e099b9defab57c04
jjjjonathan <hornathanjon@gmail.com>
2021-02-17 21:55:28 -0600

    converted book constructor to class

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 let library = [];
 
-function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-
-  this.info = function() {
-    const readText = read ? "read" : "not read yet"
-    const infoText = `${title} by ${author}, ${pages} pages, ${readText}`
-    return infoText;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
   }
 }
 
